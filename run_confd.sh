dee0a95136e4a7edfdd76ae4ef52828ef14d796e
Marc Sutter <marc.sutter@swisscom.com>
2017-01-04 16:22:11 +0100

    fix template

--- run_confd.sh
+++ run_confd.sh
@@ -5,7 +5,7 @@ echo "CONFD_LOG_LEVEL:              $CONFD_LOG_LEVEL"
 echo "CONFD_BACKEND:                $CONFD_BACKEND"
 echo "CONFD_PREFIX:                 $CONFD_PREFIX"
 echo "MOPPER_KEEP_VOLUMES_NAMED:    $MOPPER_KEEP_VOLUMES_NAMED"
-echo "MOPPER_KEEP_CONTAINERS_NAMED: $MOPPER_KEEP_VOLUMES_NAMED"
+echo "MOPPER_KEEP_CONTAINERS_NAMED: $MOPPER_KEEP_CONTAINERS_NAMED"
 echo "MOPPER_DEBUG:                 $MOPPER_DEBUG"
 echo "-------------------------------------------------------------------------------"
 exec confd --backend $CONFD_BACKEND --interval $CONFD_INTERVAL --log-level $CONFD_LOG_LEVEL --prefix $CONFD_PREFIX
