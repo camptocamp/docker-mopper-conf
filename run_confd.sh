54761ceea97db54b8b0aa87b35d7710fcd0db462
Marc Sutter <marc.sutter@swisscom.com>
2017-01-23 13:16:08 +0100

    add disable switch for unamed volumes

--- run_confd.sh
+++ run_confd.sh
@@ -12,4 +12,9 @@ echo "MOPPER_KEEP_IMAGES:           $MOPPER_KEEP_IMAGES"
 echo "MOPPER_DELAY_TIME:            $MOPPER_DELAY_TIME"
 echo "MOPPER_DEBUG:                 $MOPPER_DEBUG"
 echo "--------------------------------------------------------------------------------"
+
+if [ $CONFD_INTERVAL_IN_SECONDS -eq 0 ]; then
+    echo "Invalid interval of 0";
+    exit 1;
+fi
 exec confd --backend $CONFD_BACKEND --interval $CONFD_INTERVAL_IN_SECONDS --log-level $CONFD_LOG_LEVEL --prefix $CONFD_PREFIX
